18ebd15f22e20d809a778d2706d9faaf92f429d3
MelatSemereab <99811012+semereabmelat@users.noreply.github.com>
2022-03-30 17:36:28 +0000

    Initial commit

---
+++ SMS.Web/Program.cs
@@ -0,0 +1,43 @@
+using SMS.Data.Repository;
+using SMS.Data.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+       .AddCookie(options => {
+            options.AccessDeniedPath = "/User/ErrorNotAuthorised";
+            options.LoginPath = "/User/ErrorNotAuthenticated";
+        });
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+} else {
+    // in  development mode seed the database each time the application starts
+    StudentServiceSeeder.Seed(new StudentServiceDb());
+}
+
+//app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// ** Enable site Authentication/Authorization **
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
